00374af9f5424e2d328fb28342e9062207537d6f
KristinaKuzmenko <kristina_neskey@icloud.com>
2024-02-20 22:39:52 +0200

    Commands parcer added

--- task_1.py
+++ task_1.py
@@ -1,8 +1,9 @@
+import argparse
 from pathlib import Path
 import shutil
 
 
-def sort_files(path: Path, dist="dist"):
+def sort_files(path: Path, dist: Path):
     if path.exists():
         for obj in path.iterdir():
             if obj.is_dir():
@@ -14,7 +15,7 @@ def sort_files(path: Path, dist="dist"):
                 ext = obj.suffix.lstrip(".")
                 if not ext:
                     ext = "files_without_extension"
-                new_path = Path(dist) / ext
+                new_path = dist / ext
                 new_path.mkdir(exist_ok=True, parents=True)
                 try:
                     shutil.copyfile(obj, new_path / obj.name)
@@ -24,3 +25,21 @@ def sort_files(path: Path, dist="dist"):
 
     else:
         print("This directory doesn't exist")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("source", type=str, help="Path to the source directory.")
+    parser.add_argument(
+        "destination",
+        type=str,
+        nargs="?",
+        default="dist",
+        help="Path to the destination directory, default = dist",
+    )
+
+    args = parser.parse_args()
+    source_path = Path(args.source)
+    dest_dir = Path(args.destination)
+
+    sort_files(source_path, dest_dir)
